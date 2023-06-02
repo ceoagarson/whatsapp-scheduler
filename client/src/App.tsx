382d88e2362903cdebbe11342d9ae69486983bca
nishu kumar <kumarnishu437@gmail.com>
2023-06-02 16:25:22 +0530

    updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,25 @@
 import { useQuery } from "react-query";
 import AppRoutes from "./Routes";
 import NavBar from "./components/navbar/NavBar";
-import { GetRecords } from "./services/MessageServices";
+import { GetRecord } from "./services/MessageServices";
 import { AxiosResponse } from "axios";
 import { IRecord } from "./types/Record";
 import { BackendError } from "./types";
 import { useEffect, useState } from "react";
 
 function App() {
-  const { data, isSuccess } = useQuery<AxiosResponse<IRecord[]>, BackendError>("records", GetRecords, {
+  const [phone, setPhone] = useState(917056943283)
+  const { data, isSuccess, refetch, isLoading } = useQuery<AxiosResponse<IRecord[]>, BackendError>(["records", phone], () => GetRecord(phone), {
     refetchOnMount: true
   })
   const [records, setRecords] = useState<IRecord[]>([])
   useEffect(() => {
     if (isSuccess)
       setRecords(data.data)
-  }, [isSuccess, data])
+    if (phone) {
+      refetch()
+    }
+  }, [isSuccess, data, phone])
   console.log(records)
   return (
     <>
--- client/src/services/MessageServices.tsx
+++ client/src/services/MessageServices.tsx
@@ -5,9 +5,6 @@ export const GetMessages = async () => {
     return await apiClient.get(`messages`)
 }
 
-export const GetRecords= async () => {
-    return await apiClient.get(`records`)
-}
 export const GetRecord = async (phone:number) => {
     return await apiClient.get(`records/${phone}`)
 }
